7f363285ed76b0153b7f99e9b33115fa8079f59b
xun ma <lazybone1995@hotmail.com>
2023-02-27 16:35:37 +0800

    solution with `<template>`

--- javascript/application.js
+++ javascript/application.js
@@ -1,1 +1,15 @@
-console.log("hello from application.js")
+const movieContainer = document.querySelector('#results');
+const template = document.querySelector("#movieCardTemplate");
+
+fetch("http://www.omdbapi.com/?s=harry potter&apikey=adf1f2d7")
+.then(response => response.json())
+.then((data) => {
+    data.Search.forEach((result) => {
+      const clone = template.content.cloneNode(true);
+      clone.querySelector("img").src = result.Poster
+      clone.querySelector("h6").textContent = result.Title
+      clone.querySelector("p").textContent = result.Year
+      clone.querySelector("a").href = `https://www.imdb.com/title/${result.imdbID}`
+      movieContainer.appendChild(clone);
+    });
+  })
